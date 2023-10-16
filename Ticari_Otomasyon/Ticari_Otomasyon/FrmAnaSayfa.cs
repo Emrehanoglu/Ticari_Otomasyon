412809cef5502f3cbbb71ed262f2bb1e627916c8
Emrehanoglu <72378457+Emrehanoglu@users.noreply.github.com>
2023-10-16 19:38:53 +0300

    FrmAnaSayfa #3

--- Ticari_Otomasyon/Ticari_Otomasyon/FrmAnaSayfa.cs
+++ Ticari_Otomasyon/Ticari_Otomasyon/FrmAnaSayfa.cs
@@ -21,15 +21,23 @@ namespace Ticari_Otomasyon
 		void azalanStoklar()
 		{
 			SqlDataAdapter komut = new SqlDataAdapter("select UrunAd,sum(Adet) as Adet from Tbl_Urunler group by UrunAd " +
-														"having sum(Adet)<=15 order by sum(Adet) desc",baglan.baglanti());
+														"having sum(Adet)<=100 order by sum(Adet) desc",baglan.baglanti());
 			DataTable dt = new DataTable();
 			komut.Fill(dt);
 			gridControl1.DataSource = dt;
+		}
+		void Ajanda()
+		{
+			SqlDataAdapter komut = new SqlDataAdapter("select Tarih,Saat,Baslik from Tbl_Notlar order by Id desc", baglan.baglanti());
+			DataTable dt = new DataTable();
+			komut.Fill(dt);
+			gridControl2.DataSource = dt;
 		}
 
 		private void FrmAnaSayfa_Load(object sender, EventArgs e)
 		{
 			azalanStoklar();
+			Ajanda();
 		}
 	}
 }
